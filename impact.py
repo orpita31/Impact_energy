a345ce6de57cab067a1105afd49ada8f73b1a839
Kazi Orpita <85961722+orpita31@users.noreply.github.com>
2025-09-21 16:04:35 +0600

    Update impact.py

--- impact.py
+++ impact.py
@@ -5,11 +5,9 @@ def avg(a):
 Al_27=[64, 80, 62, 58, 56, 58]
 MS_27=[55, 125, 118, 42, 115, 67]
 MS_40=[26,8,12,14,60]
-avg_Al_27=avg(Al_27)
-avg_MS_27=avg(MS_27)
-avg_MS_40=avg(Al_27)
+
 x=['Aluminium at 27°C','Mild steel at 27°C','Mild steel at -40°C']
-y=[avg_Al_27,avg_MS_27,avg_MS_40]
+y=[avg(Al_27),avg(MS_27),avg(Al_27)]
 
 plt.bar(x,y)
 plt.title('Impact energy for different materials')
